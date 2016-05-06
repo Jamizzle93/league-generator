7653e244e647909aa74cce6577bae3341b73417d
James Warner <jamesnwarner@gmail.com>
2016-05-06 20:42:44 +0100

    refactor(*): Rename files

--- app/src/main/java/com/mysticwater/leaguegenerator/LeagueListActivity.java
+++ app/src/main/java/com/mysticwater/leaguegenerator/LeagueListActivity.java
@@ -3,11 +3,11 @@ package com.mysticwater.leaguegenerator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LeagueListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.league_list_activity);
     }
 }
