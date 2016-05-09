b088b96c351e4ff757959a7160b5b4734f037f12
James Warner <jamesnwarner@gmail.com>
2016-05-09 18:57:51 +0100

    feat(LeagueList): Add on item click listener to league list

--- app/src/main/java/com/mysticwater/leaguegenerator/LeagueListActivity.java
+++ app/src/main/java/com/mysticwater/leaguegenerator/LeagueListActivity.java
@@ -14,6 +14,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -112,6 +113,13 @@ public class LeagueListActivity extends AppCompatActivity {
 
             @Override
             public void onDestroyActionMode(ActionMode mode) {
+
+            }
+        });
+
+        mLeagueList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
             }
         });
