479c0232acfb875b7e23acad853a9c82928b5b2e
James Warner <jamesnwarner@gmail.com>
2016-05-21 09:24:16 +0100

    feat(*): Add layout setup

--- app/src/main/java/com/mysticwater/leaguegenerator/TableFragment.java
+++ app/src/main/java/com/mysticwater/leaguegenerator/TableFragment.java
@@ -1,6 +1,23 @@
 package com.mysticwater.leaguegenerator;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class TableFragment extends Fragment{
+public class TableFragment extends Fragment {
+
+    // Log tag
+    public static final String LOG_TAG = "TableFragment";
+
+    // UI Widgets
+    private View mLayoutView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mLayoutView = inflater.inflate(R.layout.team_list_fragment, container, false);
+
+        return mLayoutView;
+    }
 }
